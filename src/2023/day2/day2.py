3423d9d96ab1d1e2fd32e48cc4aa73447e6214c7
Johan Alpne <johan.alpne@gmail.com>
2023-12-02 18:17:36 +0200

    day2 cleanup

--- src/2023/day2/day2.py
+++ src/2023/day2/day2.py
@@ -12,35 +12,34 @@ def part_a():
     for game_id, cubes in games.items():
         for color, limit in limits:
             if cubes[color] > limit:
-                invalid_games.add(int(game_id))
+                invalid_games.add(game_id)
                 break
 
-    all_game_ids = set(int(game_id) for game_id in games.keys())
-    valid_games = all_game_ids - invalid_games
-
-    return sum(valid_games)
+    return sum(games.keys() - invalid_games)
 
 
 def part_b():
     data = file_parser.file_as_line_list(2023, 2)
     games = parse_cubes(data)
+
     cube_powers = []
     for cubes in games.values():
         cube_powers.append(reduce(lambda x, y: x * y, cubes.values()))
+
     return sum(cube_powers)
 
 
 def parse_cubes(data):
-    games = defaultdict(lambda: defaultdict(int))
+    games: dict[int, dict] = defaultdict(lambda: defaultdict(int))
     for line in data:
         game, info = line.split(": ")
-        _, game_id = game.split(" ")
-        cube_sets = info.split("; ")
-        for cube_set in cube_sets:
+        game_id = int(game.split(" ")[-1])
+        for cube_set in info.split("; "):
             for cube in cube_set.split(", "):
-                num, color = cube.split(" ")
-                if int(num) > games[game_id][color.strip()]:
-                    games[game_id][color.strip()] = int(num)
+                num, color = cube.strip().split(" ")
+                num = int(num)
+                if num > games[game_id][color]:
+                    games[game_id][color] = num
     return games
 
 
